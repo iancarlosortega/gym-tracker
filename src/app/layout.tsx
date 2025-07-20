252e8a5d839c689767149d3c6eb5446fe8050a70
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2025-07-20 10:43:35 -0500

    feat: setup husky, lint staged, biome and commitlint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,25 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
+	subsets: ['latin'],
+	weight: ['400', '600', '700'],
 });
 
 export const metadata: Metadata = {
-  title: "Gym Tracker",
-  description: "Track your gym workouts",
+	title: 'Gym Tracker',
+	description: 'Track your gym workouts',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${poppins.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${poppins.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
